14817048d91a48608e6d1c6fd93aeb9aadbe4e3d
Ariel Nardon <arinardon@Ariels-MacBook-Pro.local>
2025-03-06 22:03:09 -0300

    Move frontend files to /frontend/ and clean up root

--- tailwind.config.js
+++
@@ -1,8 +0,0 @@
-/** @type {import('tailwindcss').Config} */
-export default {
-  content: ["./index.html", "./src/.{js,ts,jsx,tsx}"], // Asegúrate de que los archivos están incluidos
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-};
